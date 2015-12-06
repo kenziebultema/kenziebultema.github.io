0bedef254537487fe3cb2d16158054745ff10fac
Kenzie Bultema <krbultema@gmail.com>
2015-12-06 16:39:04 -0600

    possibly finished

--- application.js
+++ application.js
@@ -1,8 +1,26 @@
 $(document).ready(function() {
-        $('.welcome').on('click', 'button', function() {
-        $('body').find('header').show();
-        $('body').find('section').show();
-        $('body').find('footer').show();
-        $('button').find('nav').show();
-      });
+  $('.welcome').on('click', 'button', function() {
+    $('body').find('header').show();
+    $('body').find('section').show();
+    $('body').find('footer').show();
+    $('body').find('nav').show();
+    $(this).hide();
+    $('.art').find('button').show();
+  });
+  $('.art').on('click', 'button', function() {
+    $('body').find('header').hide();
+    $('body').find('section').hide();
+    $('body').find('footer').hide();
+    $('body').find('nav').hide();
+    $(this).hide();
+    $('.done').find('button').show();
+  });
+  $('.done').on('click', 'button', function() {
+    $('body').find('header').show();
+    $('body').find('section').show();
+    $('body').find('footer').show();
+    $('body').find('nav').show();
+    $(this).hide();
+    $('.art').find('button').show();
+  });
 });
