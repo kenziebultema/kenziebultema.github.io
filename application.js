dd83276a9bbb5915ed1b85c99845ab5d163d09c7
Kenzie Bultema <krbultema@gmail.com>
2016-03-02 15:14:25 -0600

    move jquery to specific functions, add info to education and credit tabs(forgot a year i lettered academically and added linkedin profile link)

--- application.js
+++ application.js
@@ -1,26 +1,23 @@
-$(document).ready(function() {
-  $('.welcome').on('click', 'button', function() {
+$(document).ready(function(){
+    $('.welcome').on('click', 'button', showResume);
+    $('.art').on('click', 'button', hideResume);
+    $('.done').on('click', 'button', showResume);
+});
+
+function showResume(){
     $('body').find('header').show();
     $('body').find('section').show();
     $('body').find('footer').show();
     $('body').find('nav').show();
     $(this).hide();
     $('.art').find('button').show();
-  });
-  $('.art').on('click', 'button', function() {
+}
+
+function hideResume(){
     $('body').find('header').hide();
     $('body').find('section').hide();
     $('body').find('footer').hide();
     $('body').find('nav').hide();
     $(this).hide();
     $('.done').find('button').show();
-  });
-  $('.done').on('click', 'button', function() {
-    $('body').find('header').show();
-    $('body').find('section').show();
-    $('body').find('footer').show();
-    $('body').find('nav').show();
-    $(this).hide();
-    $('.art').find('button').show();
-  });
-});
+}
