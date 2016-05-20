3a9eb931be5bbc1bac5b0a9e6f521c065eb024ee
Kenzie Bultema <krbultema@gmail.com>
2016-05-19 22:06:22 -0500

    maybe works

--- application.js
+++ application.js
@@ -1,24 +1,26 @@
-
-$(document).ready(function(){
-    $('.welcome').on('click', 'button', showResume);
-    $('.art').on('click', 'button', hideResume);
-    $('.done').on('click', 'button', showResume);
-});
-
-function showResume(){
+$(document).ready(function() {
+  $('.welcome').on('click', 'button', function() {
     $('body').find('header').show();
     $('body').find('section').show();
     $('body').find('footer').show();
     $('body').find('nav').show();
     $(this).hide();
     $('.art').find('button').show();
-};
-
-function hideResume(){
+  });
+  $('.art').on('click', 'button', function() {
     $('body').find('header').hide();
     $('body').find('section').hide();
     $('body').find('footer').hide();
     $('body').find('nav').hide();
     $(this).hide();
     $('.done').find('button').show();
-};
+  });
+  $('.done').on('click', 'button', function() {
+    $('body').find('header').show();
+    $('body').find('section').show();
+    $('body').find('footer').show();
+    $('body').find('nav').show();
+    $(this).hide();
+    $('.art').find('button').show();
+  });
+});
